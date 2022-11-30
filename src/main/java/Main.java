ac72159691c4662e3127abe7d240df4ca97cf571
Kadir <kadirgucuyener35@gmail.com>
2022-11-30 19:52:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Bir Yıl Girin:");
+        year=input.nextInt();
+
+        if (year%100==0){
+            if (year%400==0){
+                System.out.println(year+" bir artık yıldır.");
+            }else {
+                System.out.println(year+" bir artık yıl değildir. ");
+            }
+        }
+        else if (year%4==0){
+            System.out.println(year+" bir artık yıldır.");
+        }else {
+            System.out.println(year+" bir artık yıl değildir.");
+        }
+    }
+}
